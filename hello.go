b566c62ab549af67fd7c732dc5ffa3861a63c8e4
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-01-15 22:10:52 -0300

    Update hello.go

--- hello.go
+++ hello.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-);
+)
 
 func main() {
 	var language string = "Learning Go!"
@@ -22,7 +22,7 @@ func main() {
 	fmt.Println("The user is: ", userInfo, "Shortname:", name[0:3])
 	fmt.Println(language, len(language), "is the amount of bytes in memory")
 	fmt.Println("A copy", copy)
-	
+
 	// If I know the amount of elements in an array, and the elements must be of the same type
 	// Arrays cannot be resized
 	var myarray = [3]string{"first", "second", "third"}
@@ -42,4 +42,36 @@ func main() {
 	myslice := make([]string, 3)
 	myslice = append(myslice, "three", "four")
 
+	// we can modify the slice's capacity so that we can extend it.
+	// We can specify the capacity adding a third parameter to make():
+	var some_names = make([]string, 0, 10)
+	some_names = append(some_names, "elias", "pedro", "martin", "pablito")
+
+	last_two_names := some_names[:2]
+	first_two_names := some_names[2:]
+	middle_names := some_names[1:3]
+
+	fmt.Println(some_names)
+	fmt.Println(last_two_names)
+	fmt.Println(first_two_names)
+	fmt.Println(middle_names)
+
+	// a map does not need an amount of items
+	agesMap := make(map[string]int)
+	agesMap["elias"] = 25
+	// or it can be written in this way
+	ageMap := map[string]int{"elias": 25}
+	fmt.Println(ageMap)
+	delete(agesMap, "elias")
+
+	// In go there are no whiles, there are only for loops
+	var numbers = make([]int, 10)
+	for i := 0; i < 10; i++ {
+		numbers = append(numbers, i)
+	}
+
+	// to iteare an array
+	for i, num := range numbers {
+		fmt.Printf("Number: %d - Position: %d\n", i, num)
+	}
 }
